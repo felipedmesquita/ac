561a62eb6a54fe4c40419f8cbb7322ad61d15b87
marialuiza1324 <marialuiza.nerypinto@yahoo.com.br>
2025-03-06 09:44:22 -0300

    refactor authenticate

--- lib/ac/base.rb
+++ lib/ac/base.rb
@@ -9,16 +9,24 @@ module Ac
     end
 
     def url path
-      File.join(self.class::BASE_URL, path)
+      if path.start_with?("http://") || path.start_with?("https://")
+        path
+      else
+        File.join(self.class::BASE_URL, path)
+      end
     end
+
+     def authenticate! options
+      if access_token
+        options[:headers] ||= {}
+        options[:headers]["Authorization"] = "Bearer #{access_token}"
+      end
+     end
 
     %i[get post put patch delete].each do |http_verb|
       define_method :"#{http_verb}_request" do |path, options = {}|
         options[:method] = http_verb
-        if access_token
-          options[:headers] ||= {}
-          options[:headers]["Authorization"] = "Bearer #{access_token}"
-        end
+        authenticate!(options) unless options.delete(:skip_authentication)
         Typhoeus::Request.new url(path), options
       end
 
