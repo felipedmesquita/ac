7639dfdcabfad2978f024010f991cd5e9b66d89f
marialuiza1324 <marialuiza.nerypinto@yahoo.com.br>
2025-03-11 16:35:12 -0300

    authenticate + retry

--- lib/ac/base.rb
+++ lib/ac/base.rb
@@ -3,9 +3,14 @@ module Ac
     MAX_RETRIES = 3
     SAVE_RESPONSES = false
 
-    attr_reader :access_token
+    attr_reader :token_expires_at
     def initialize access_token = nil
       @access_token = access_token
+    end
+
+    def access_token
+      refresh_token if respond_to?(:refresh_token) && (@access_token.blank? || @token_expires_at&.past?)
+      @access_token
     end
 
     def url path
@@ -32,26 +37,43 @@ module Ac
 
       define_method http_verb do |path, options = {}, &block|
         retries_count ||= 0
-        raise "Too many retries" if retries_count > self.class::MAX_RETRIES
-        # puts "Requesting #{path}, retry number #{retries_count}"
+
         response = send(:"#{http_verb}_request", path, options).run
+        Database.save_request response, class_name: self.class.name if self.class::SAVE_RESPONSES
 
         if block
-          begin
-            raise unless block.call(response)
-            Database.save_request response, class_name: self.class.name if self.class::SAVE_RESPONSES
-          rescue
-            Database.save_request(response, class_name: self.class.name + "_errors") if self.class::SAVE_RESPONSES
-            retries_count += 1 # standard:disable Lint/UselessAssignment
-            sleep(2**retries_count)  # Exponential backoff
-            redo
+          unless run_block_validation(AcObject.from_response(response), block)
+            if retries_count < self.class::MAX_RETRIES
+              sleep(2**retries_count)
+              retries_count += 1
+              redo
+            else
+              raise BlockValidationError.new(AcObject.from_response(response))
+            end
           end
-        elsif self.class::SAVE_RESPONSES
-          Database.save_request response, class_name: self.class.name
+        else
+          unless response.success?
+            if can_retry?(response) && retries_count < self.class::MAX_RETRIES
+              sleep(2**retries_count)
+              retries_count += 1
+              redo
+            else
+              raise AcError.from_response(AcObject.from_response(response))
+            end
+          end
         end
-
-        response
+        return AcObject.from_response(response)
       end
+    end
+
+    def run_block_validation response, block
+      block.call response
+    rescue StandardError => e
+      false
+    end
+
+    def can_retry? response
+      response.timed_out? || response.code == 0 || response.code == 429 || response.code >= 500
     end
   end
 end
